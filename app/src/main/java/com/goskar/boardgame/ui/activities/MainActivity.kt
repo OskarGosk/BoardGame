628f617c5cf4226ce80685b50d2c80f6a1ba90c4
oskargosk <gosk60@gmail.com>
2025-01-11 22:44:14 +0100

    Add color schema

--- app/src/main/java/com/goskar/boardgame/ui/activities/MainActivity.kt
+++ app/src/main/java/com/goskar/boardgame/ui/activities/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.goskar.boardgame.ui.ComposeApp
+import com.goskar.boardgame.ui.theme.BoardGameTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeApp()
+            BoardGameTheme {
+                ComposeApp()
+            }
+//            ComposeApp()
         }
     }
 }
