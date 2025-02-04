12c920b219b483a3733040d3ed063184927aa1db
Oskar Gosk <ogosk@9bits.pl>
2025-02-04 13:44:34 +0100

    Start add photo,

--- app/src/main/java/com/goskar/boardgame/ui/activities/MainActivity.kt
+++ app/src/main/java/com/goskar/boardgame/ui/activities/MainActivity.kt
@@ -1,18 +1,52 @@
 package com.goskar.boardgame.ui.activities
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.goskar.boardgame.ui.ComposeApp
 import com.goskar.boardgame.ui.theme.BoardGameTheme
 
 class MainActivity : ComponentActivity() {
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted){
+            Log.i("Camera","Permission Granted")
+        } else {
+            Log.i("Camera","Permission Denied")
+
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BoardGameTheme {
                 ComposeApp()
             }
+        }
+
+        requestCameraPermission()
+    }
+
+    private fun requestCameraPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED -> Log.i ("Camera", "Permission previously granted")
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) -> Log.i ("Camera", "Show camera permission dialog")
+
+            else -> requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
         }
     }
 }
