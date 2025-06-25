298136dcb823b462492bd407f9258f615f04a066
Goskar <oskar.a.gosk@gmail.com>
2025-06-25 20:07:35 +0200

    refactor

--- app/src/main/java/com/goskar/boardgame/ui/login/LoginScreen.kt
+++ app/src/main/java/com/goskar/boardgame/ui/login/LoginScreen.kt
@@ -9,11 +9,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -23,7 +28,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -40,6 +47,8 @@ import com.goskar.boardgame.ui.components.scaffold.BoardGameScaffold
 import com.goskar.boardgame.ui.home.HomeScreen
 import com.goskar.boardgame.ui.theme.Smooch16
 import com.goskar.boardgame.ui.theme.Smooch20
+import com.goskar.boardgame.utils.Keyboard
+import com.goskar.boardgame.utils.keyboardAsState
 import org.koin.androidx.compose.koinViewModel
 
 class LoginScreen : Screen {
@@ -89,17 +98,19 @@ fun LoginContent(
     questLogIn: () -> Unit = {},
     paddingValues: PaddingValues
 ) {
-
-    val passwordVisible by remember { mutableStateOf(false) }
+    var passwordVisible by remember { mutableStateOf(false) }
+    val isKeyboardOpen by keyboardAsState()
 
     if (state.isLoading) AppLoader()
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(paddingValues)
+            .verticalScroll(rememberScrollState())
             .padding(horizontal = 10.dp),
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = if (isKeyboardOpen == Keyboard.Opened) Arrangement.Top else Arrangement.Center
     ) {
+        if (isKeyboardOpen == Keyboard.Opened) Spacer(modifier = Modifier.height(100.dp))
         OutlinedTextField(
             modifier = Modifier
                 .fillMaxWidth()
@@ -143,6 +154,20 @@ fun LoginContent(
             keyboardActions = KeyboardActions(onDone = {
                 if (state.login.isNotEmpty() && state.password.isNotEmpty()) logIn()
             }),
+            trailingIcon = {
+                IconButton(
+                    onClick = {
+                        passwordVisible = !passwordVisible
+                    }
+                ) {
+                    Icon(
+                        painter = painterResource(if (!passwordVisible) R.drawable.icons_visible else R.drawable.icons_invisible),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(20.dp)
+                    )
+                }
+            },
             visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
             label = {
                 Text(
@@ -186,9 +211,7 @@ fun LoginContent(
                 }
             }
 
-
         }
-
     }
 }
 
